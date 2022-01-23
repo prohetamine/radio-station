8e13c3144e44c02024fcab110627ea3a58e380ae
Stas Prohetamine <prohetamine@gmail.com>
2022-01-23 15:25:15 +0300

    save

--- src/stream-helper.js
+++ src/stream-helper.js
@@ -30,7 +30,7 @@ const StreamHelper = async ({ isStart }) => {
     listeners.push(res)
   }
 
-  const browser = await puppeteer.launch({ headless: false })
+  const browser = await puppeteer.launch({ args: ['--no-sandbox'] })
   const page = await browser.newPage()
 
   await page.addScriptTag({ url: 'https://cdn.jsdelivr.net/npm/sfmediastream@v1' })
